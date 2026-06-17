ae9d7bc3c6e09f606be98ec3c604a8cddf45fc4a
unknown <gobers107@gmail.com>
2026-06-17 14:55:54 +0300

    add new item

---
+++ src/main/java/org/gobers/teftercraft/item/custom/TeftnrfItem.java
@@ -0,0 +1,8 @@
+package org.gobers.teftercraft.item.custom;
+import net.minecraft.world.item.Item;
+
+public class TeftnrfItem extends Item {
+    public TeftnrfItem(Properties properties) {
+		super(properties);
+    }
+}
